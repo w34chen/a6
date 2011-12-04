0f11d04f9d9fac2b7e5a0c21b5698dc1d568dba7
Weiliang Chen <w34chen@uwaterloo.ca>
2011-12-04 01:56:41 -0500

    Student with basic structure

--- Student.cc
+++ Student.cc
@@ -2,6 +2,8 @@
 
 #include "Student.h"
 #include "NameServer.h"
+#include "WATCard.h"
+#include "VendingMachine.h"
 #include "mprng.h"
 
 extern MPRNG mprng_;
@@ -9,33 +11,49 @@ extern MPRNG mprng_;
 void Student::main() {
   // Generate how many times student is going to purchase soda
   // range: [1, MaxPurchase]
+  unsigned int numOfSoda = mprng_(1, maxPurchases);
   
   // Select a random flavor [0,3]
+  VendingMachine::Flavours favFlavour = (VendingMachine::Flavours)mprng_(0,3);
 
   // Create a new WATCard with $5 balance
+  WATCard* card;
+  FWATCard fCard = cardOffice.create(id, 5, card);
 
   // Obtain location to vending machine from name server
-
+  VendingMachine* vendingMachine = nameServer.getMachine(id);
+  
   // Loop through all the attempts to purchase soda
+  for (unsigned int i = 0; i < numOfSoda; i++) {
+    // Yield before purchase soda
+    unsigned int yieldTime = mprng_(1, 10);
+    yield(yieldTime);
   
-  // Yield before purchase soda
-  
-  // Purchase sode from vending machine
+    while (true) { // Will busy waiting to try buy soda unless succeeded
+      try {
+	_Enable {
+	  // Purchase sode from vending machine
 
-  // Drink soda
+	  // Insufficient funds
 
-  // Insufficient funds
+	  // Transfer soda-code plus $5 to WATCard via WATCard office
 
-  // Transfer soda-code plus $5 to WATCard via WATCard office
+	  // Vending machine out of flavor
 
-  // Vending machine out of flavor
+	  // Obtain new vending machine from name server
 
-  // Obtain new vending machine from name server
+	}
+      } catch (WATCardOffice::Lost) {
+	// WATCard is lost
+	// Delete old WATCard
+	// Create new WATCard
 
-  // WATCard is lost
-  // Delete old WATCard
-  // Create new WATCard
+      }
+    } // while
+  L1:; // Successfully bought soda
+    // Drink soda
 
+  } // for
 }
 
 
