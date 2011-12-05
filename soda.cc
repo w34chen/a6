9a5caf2a8d2129fc7ec59c4c6df81ee1ba0ef22e
Weiliang Chen <w34chen@uwaterloo.ca>
2011-12-05 16:08:14 -0500

    Add config file implementation for umain

--- soda.cc
+++ soda.cc
@@ -1,3 +1,6 @@
+#include <uC++.h>
+#include <sstream>
+
 #include "soda.h"
 #include "Printer.h"
 #include "WATCard.h"
@@ -8,6 +11,7 @@
 #include "Bank.h"
 #include "Student.h"
 #include "Parent.h"
+#include "config.h"
 
 #include "mprng.h"
 
@@ -15,43 +19,70 @@ MPRNG mprng_;
 
 using namespace std;
 
+bool convert( unsigned int &val, char *buffer ) {		// convert C string to integer
+    std::stringstream ss( buffer );			// connect stream and buffer
+    ss >> dec >> val;					// convert integer from buffer
+    return ! ss.fail() &&				// conversion successful ?
+	// characters after conversion all blank ?
+	string( buffer ).find_first_not_of( " ", ss.tellg() ) == string::npos;
+} // convert
+
+void usage( char *argv[] ) {
+  cerr << "Usage: " << argv[0] << " [ config-file [ random-seed ] ]" << endl;
+  exit( EXIT_FAILURE );				// TERMINATE
+} // usage
+
 void uMain::main() {
 
-	unsigned int numStudents=2, numVendingMachines=2, numCouriers=1;
-	unsigned int maxPurchases = 3, parentalDelay = 20;
-	unsigned int SodaCost = 1, MaxStockPerFlavour = 5, MaxShippedPerFlavour = 6, TimeBetweenShipments = 5;
-	
-	unsigned int seed = 3;
+  unsigned int seed = getpid();
+  ConfigParms config;
+  
+  switch ( argc ) {
+  case 3:
+    if ( ! convert( seed , argv[2] ) || seed < 0) {	// invalid integer ?
+      usage( argv );
+	} // if
+    // FALL THROUGH
+  case 2:
+    processConfigFile( argv[1], config );
+    break;
+  case 1:
+    processConfigFile( "soda.config", config);
+    break;
+    // all defaults
+  default:						// wrong number of options
+    usage( argv );
+  } // switch
 
 	mprng_.seed(seed);
 
-	Printer prt(numStudents,numVendingMachines,numCouriers);
+	Printer prt(config.numStudents,config.numVendingMachines,config.numCouriers);
 
-	Bank bank(numStudents);
+	Bank bank(config.numStudents);
 
-	Parent parent(prt, bank, numStudents, parentalDelay );
+	Parent parent(prt, bank, config.numStudents, config.parentalDelay );
 
-	WATCardOffice cardOffice(prt, bank, numCouriers ); //the couriers are created by the WATCard ofﬁce.
+	WATCardOffice cardOffice(prt, bank, config.numCouriers ); //the couriers are created by the WATCard ofﬁce.
 
-	NameServer nameServer(prt, numVendingMachines, numStudents );
+	NameServer nameServer(prt, config.numVendingMachines, config.numStudents );
 
-	VendingMachine *vendingMachine[numVendingMachines];
-	for (unsigned int i = 0; i < numVendingMachines; i++)
-		vendingMachine[i] = new VendingMachine(prt, nameServer, i, SodaCost, MaxStockPerFlavour );
-	cout <<"umain: max stock per flavour: " <<MaxStockPerFlavour <<endl;
-	BottlingPlant *bottlingPlant = new BottlingPlant(prt, nameServer, numVendingMachines,MaxShippedPerFlavour,
-			MaxStockPerFlavour, TimeBetweenShipments ); //truck is created by the bottling plant
+	VendingMachine *vendingMachine[config.numVendingMachines];
+	for (unsigned int i = 0; i < config.numVendingMachines; i++)
+		vendingMachine[i] = new VendingMachine(prt, nameServer, i, config.sodaCost, config.maxStockPerFlavour );
+	cout <<"umain: max stock per flavour: " <<config.maxStockPerFlavour <<endl;
+	BottlingPlant *bottlingPlant = new BottlingPlant(prt, nameServer, config.numVendingMachines,config.maxShippedPerFlavour,
+			config.maxStockPerFlavour, config.timeBetweenShipments ); //truck is created by the bottling plant
 
-	Student *student[numStudents];
-	for (unsigned int i = 0; i < numStudents; i++)
-		student[i] = new Student(prt, nameServer, cardOffice, i, maxPurchases );
+	Student *student[config.numStudents];
+	for (unsigned int i = 0; i < config.numStudents; i++)
+		student[i] = new Student(prt, nameServer, cardOffice, i, config.maxPurchases );
 
-	for (unsigned int i = 0; i < numStudents; i++)
+	for (unsigned int i = 0; i < config.numStudents; i++)
 		delete(student[i]);
 
 	delete(bottlingPlant);
 
-	for (unsigned int i = 0; i < numVendingMachines; i++)
+	for (unsigned int i = 0; i < config.numVendingMachines; i++)
 		delete(vendingMachine[i]);
 
 
